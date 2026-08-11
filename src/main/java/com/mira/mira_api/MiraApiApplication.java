0635ef3041a82d3c2341daa2fd7e9629522a15c4
vitor4818 <vitorg.martins4818@gmail.com>
2026-08-10 21:37:35 -0300

    chore: initial project setup for MIRA service

---
+++ src/main/java/com/mira/mira_api/MiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.mira.mira_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mira/mira_api/MiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mira.mira_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
